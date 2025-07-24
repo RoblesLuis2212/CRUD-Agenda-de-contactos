b0b2ff1e5617d82058c0bffe2e0cf31aa76d7e32
Luis Robles <luisgeremiasrobles@gmail.com>
2025-07-24 19:48:51 -0300

    Agregamos la clase contacto

---
+++ js/Contacto.js
@@ -0,0 +1,113 @@
+class Contacto {
+  #id;
+  #nombre;
+  #apellido;
+  #telefono;
+  #email;
+  #imagen;
+  #empresa;
+  #puestoTrabajo;
+  #direccion;
+  #notas;
+
+  constructor(
+    id,
+    nombre,
+    apellido,
+    telefono,
+    email,
+    imagen,
+    empresa,
+    puestoTrabajo,
+    direccion,
+    notas
+  ) {
+    this.#id = id;
+    this.#nombre = nombre;
+    this.#apellido = apellido;
+    this.#telefono = telefono;
+    this.#email = email;
+    this.#imagen = imagen;
+    this.#empresa = empresa;
+    this.#puestoTrabajo = puestoTrabajo;
+    this.#direccion = direccion;
+    this.#notas = notas;
+  }
+  get id() {
+    return this.#id;
+  }
+  get nombre() {
+    return this.#nombre;
+  }
+  get apellido() {
+    return this.#apellido;
+  }
+  get telefono() {
+    return this.#telefono;
+  }
+  get email() {
+    return this.#email;
+  }
+  get imagen() {
+    return this.#imagen;
+  }
+  get empresa() {
+    return this.#empresa;
+  }
+  get puestoTrabajo() {
+    return this.#puestoTrabajo;
+  }
+  get direccion() {
+    return this.#direccion;
+  }
+  get notas() {
+    return this.#notas;
+  }
+
+  // Setters
+  set id(nuevoId) {
+    this.#id = nuevoId;
+  }
+  set nombre(nuevoNombre) {
+    this.#nombre = nuevoNombre;
+  }
+  set apellido(nuevoApellido) {
+    this.#apellido = nuevoApellido;
+  }
+  set telefono(nuevoTelefono) {
+    this.#telefono = nuevoTelefono;
+  }
+  set email(nuevoEmail) {
+    this.#email = nuevoEmail;
+  }
+  set imagen(nuevaImagen) {
+    this.#imagen = nuevaImagen;
+  }
+  set empresa(nuevaEmpresa) {
+    this.#empresa = nuevaEmpresa;
+  }
+  set puestoTrabajo(nuevoTrabajo) {
+    this.#puestoTrabajo = nuevoTrabajo;
+  }
+  set direccion(nuevaDireccion) {
+    this.#direccion = nuevaDireccion;
+  }
+  set notas(nuevaNotas) {
+    this.#notas = nuevaNotas;
+  }
+  //Metodo para almacenar el objeto en el localStorage/SessionStorage
+  toJSON() {
+    return {
+      id: this.#id,
+      nombre: this.#nombre,
+      apellido: this.#apellido,
+      telefono: this.#telefono,
+      email: this.#email,
+      imagen: this.#imagen,
+      empresa: this.#empresa,
+      puestoTrabajo: this.#puestoTrabajo,
+      direccion: this.#direccion,
+      notas: this.#notas,
+    };
+  }
+}
